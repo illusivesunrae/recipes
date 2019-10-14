24259b636c932f6f063e7fc60a8f1297763a8a9d
Rachel <racmocon@gmail.com>
2019-10-14 19:42:16 -0400

    Added webpack boilerplate base

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+const pkg = require('./package.json');
+const banner = pkg.name + ` - @version ` + pkg.version + `
+
+Copyright (C) 2019 Rachel O'Connor
+SPDX-License-Identifier: BSD-3-Clause`;
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'var',
+    library: 'Component'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    index: 'index.html'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin(banner)
+  ]
+};
